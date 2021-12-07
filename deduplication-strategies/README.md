10cf312c17c6a20c65a2a68cd4d5076303f793c1
gnzjgo <gonzalojgo@gmail.com>
2021-12-07 12:44:45 +0100

    click options

--- deduplication-strategies/README.md
+++ deduplication-strategies/README.md
@@ -1,4 +1,19 @@
 ## Data Generator for Deduplication Strategies
 The script generates a CSV file with 5 columns: _'id','views','likes','dislikes','timestamp'_
-    
-The total possible number of ids is set by _TOTAL_VIDEOS_, the number of rows generated is set by _ROWS_
+
+### Install
+`pip install click`
+
+### Run script
+`python3 gen.py --rows 50_000 --total-videos 1_000_000`
+
+this generates a csv file with 50k rows with ids from 1 to 1.000.000.
+
+### Parameters
+the folowing parameters can be specified, although none of them is required.
+- __rows__ : number of generated rows
+- __total-videos__: total number of videos
+- __file-name__: name of the output csv file
+
+### Help
+run  `python3 gen.py --rows 50_000 --total-videos 1_000_000`  for help
--- deduplication-strategies/data-generator/gen.py
+++ deduplication-strategies/data-generator/gen.py
@@ -1,6 +1,7 @@
 import datetime
 import csv
 import random
+import click
 
 ROWS = 500_000
 TOTAL_VIDEOS = 1_000_000
@@ -13,20 +14,31 @@ timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
 
 FILE_NAME = f'data_{ROWS}_{now.strftime("%Y-%m-%d_%H:%M:%S")}.csv'
 
-# print(datetime.datetime.now())
 
-def generate_csv():
-  with open (FILE_NAME,'w') as csvfile:
+@click.command()
+@click.option('--rows', default=ROWS, help=f'number of generated rows. Defaults to {ROWS}')
+@click.option('--total-videos', default=TOTAL_VIDEOS, help=f'total number of videos. Defaults to {TOTAL_VIDEOS}')
+@click.option('--file-name', default='', help= 'name of the output csv file. Defaults to "rows_ROWS_over_TOTAL_VIDEOS_datetime"')
+def generate_csv(rows, total_videos, file_name):
+  if total_videos < rows:
+    total_videos = rows
+    video_sample = range(rows)
+  else:
+    video_sample = random.sample(range(total_videos),min(rows,total_videos))
+
+  if not file_name:
+    file_name = f'rows_{rows}_over_{total_videos}_{now.strftime("%Y-%m-%d_%H:%M:%S")}.csv'
+
+  with open (file_name,'w') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(headers)
-    for r in random.sample(range(TOTAL_VIDEOS),min(ROWS,TOTAL_VIDEOS)):
+    for r in video_sample:
       id = 1+r
       views = int(delta + 1000*random.lognormvariate(5,1))
       likes = int(delta/10 + 10*random.lognormvariate(5,1))
       dislikes = int(delta/100 + random.lognormvariate(5,1)) 
       writer.writerow([id, views, likes, dislikes, timestamp])
-  print (FILE_NAME,' generated')
-  # print(datetime.datetime.now())
-
+  print (file_name,' generated')
+ 
 if __name__ == '__main__':
   generate_csv()
